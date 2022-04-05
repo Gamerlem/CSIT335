9d4a985506ef6a4e96bfeffc7c5d18ae00a65b20
Mel Jay Llanos <mel.jay.llanos@gmail.com>
2022-04-05 16:05:36 +0800

    fix

--- Scenario1.py
+++ Scenario1.py
@@ -6,12 +6,8 @@ class TestScenario1(unittest.TestCase):
     def setUp(self) -> None:
         profile1 = Profile(1, "Jc", 22, "Talisay", "Shemale", True)
         profile2 = Profile(2, "Secret", 22, "Talisay", "Female", False)
-
-        # pre-condition of second profile
         profile2.addToLikeList(profile1.id)
-
         self.match = Match(profile1, profile2, True)
-
         return super().setUp()
 
     def tearDown(self) -> None:
