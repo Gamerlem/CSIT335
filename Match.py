0da0658c9f691dc3472cedf2272d617baa36dff2
Mel Jay Llanos <mel.jay.llanos@gmail.com>
2022-04-05 17:29:37 +0800

    scenario 9 to 16

--- Match.py
+++ Match.py
@@ -36,43 +36,43 @@ class Match():
         return self.profile1.isValid and self.profile2.isValid
 
     def checkPremium(self):
-        return self.profile1.isPremium
+        return self.profile1.isPremium and self.checkValidAccounts()
 
        # check if other users like the main user
     def isLikedProfile(self):
-        return self.profile1.id in self.profile2.likeList
+        return self.profile1.id in self.profile2.likeList and self.checkValidAccounts()
 
     # check if main user likes the other user
     def isLikeProfile(self):
-        return self.isLike
+        return self.isLike and self.checkValidAccounts()
 
     def addToLikeList(self):
-        if self.checkValidAccounts() and self.isLike:
+        if self.checkValidAccounts() and self.isLike and self.checkValidAccounts():
             self.profile1.addToLikeList(self.profile2.id)
             return True
         return False
 
     def addToMatchList(self):
-        if self.isLikedProfile() and self.isLikeProfile():
+        if self.isLikedProfile() and self.isLikeProfile() and self.checkValidAccounts():
             self.profile1.addToMatchList(self.profile2.id)
             self.profile2.addToMatchList(self.profile1.id)
             return True
         return False
 
     def addToRewindList(self):
-        if not self.isLikeProfile():
+        if not self.isLikeProfile() and self.checkValidAccounts():
             self.profile1.addToRewindList(self.profile2.id)
             return True
         return False
 
     def notifyMatch(self):
-        if self._isMatch():
+        if self._isMatch() and self.checkValidAccounts():
             print("It's a Match")
             return True
         return False
 
     def allowConnection(self):
-        if self._isMatch() or (self.profile1.isPremium and self.isLike):
+        if (self._isMatch() or (self.profile1.isPremium and self.isLike)) and self.checkValidAccounts():
             print("You can message now.")
             return True
         return False
